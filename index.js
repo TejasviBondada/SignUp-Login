d139b4c15a700fd1cad3e5856989d4d72be20ec6
TejasviB <tejasvibondada@gmail.com>
2025-11-14 20:46:12 +0530

    done

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ function checkUser(str){
         return "User already exists,Please login.";
     }
     else{
+        validUsers.push(str.toLowerCase());
         return "Sign Up successful,Please login";
+    }
+}
+
+function login(username, password){
+    const validUsers = ["alice", "bob", "charlie"];
+    if(validUsers.includes(username.toLowerCase()) && password === "Emp@123"){
+        return "Login successful";
+    }
+    else if(validUsers.includes(username.toLowerCase()) && password != "Emp@123"){
+        return "Wrong password";
+    }
+    else{
+        return "User not found,Please sign up.";
     }
 }
